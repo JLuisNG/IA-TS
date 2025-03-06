2d1180808304d12bef98c0b9727c423934f42eb4
JLuisNG <jorgex4p@gmail.com>
2025-03-06 17:17:44 -0500

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import LoginCard from './pages/login/LoginCard';
-import HomePage from './pages/welcome/HomePage';
+import HomePage from './pages/welcome/Welcome';
 // Importar FontAwesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
