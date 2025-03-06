d0a710ca4176c409b5bd2d7d02cee63a2c1e5c03
JLuisNG <jorgex4p@gmail.com>
2025-03-06 17:06:32 -0500

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import LoginCard from './pages/login/LoginCard';
-import HomePage from './pages/homePage/HomePage';
+import HomePage from './pages/welcome/HomePage';
 // Importar FontAwesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -10,7 +10,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<LoginCard />} />
-        <Route path="#/welcome" element={<HomePage />} />
+        <Route path="/welcome" element={<HomePage />} />
       </Routes>
     </HashRouter>
   );
